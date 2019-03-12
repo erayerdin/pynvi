522a55e4052bd3cb22d50e616f57eb1b3e8838aa
Eray Erdin <eraygezer.94@gmail.com>
2019-03-12 17:52:09 +0300

    implemented verify_identity fully

--- pynvi/__init__.py
+++ pynvi/__init__.py
@@ -9,11 +9,51 @@ kütüphanesidir.
 __author__ = "Eray Erdin"
 __version__ = "0.1.0pre1"
 
+URL = "https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx"
 
-def verify_identity():
+
+def verify_identity(
+    identity_number: int, name: str, surname: str, year_of_birth: int
+) -> bool:
     """
     `TCKimlikNoDogrula` servisine denk gelen metod.
 
     Girdilerle ilgili kişinin TC vatandaşı olup olmadığını doğrular.
     """
-    pass
+    identity_number = int(identity_number)
+    name = str(name)
+    surname = str(surname)
+    year_of_birth = int(year_of_birth)
+
+    request_body = """
+<?xml version="1.0" encoding="utf-8"?>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+    <TCKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
+      <TCKimlikNo>{identity_number}</TCKimlikNo>
+      <Ad>{name}</Ad>
+      <Soyad>{surname}</Soyad>
+      <DogumYili>{year_of_birth}</DogumYili>
+    </TCKimlikNoDogrula>
+  </soap12:Body>
+</soap12:Envelope>
+    """.strip().format(
+        identity_number=identity_number,
+        name=name,
+        surname=surname,
+        year_of_birth=year_of_birth,
+    )
+    response = requests.post(
+        URL,
+        data=request_body.encode("utf-8"),
+        headers={"Content-Type": "application/soap+xml; charset=utf-8"},
+    )
+    response_body = response.text
+
+    if response.status_code == 200:
+        return (
+            "<TCKimlikNoDogrulaResult>true</TCKimlikNoDogrulaResult>"
+            in response_body
+        )
+
+    raise Exception(response_body)
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,16 +1,52 @@
-import pynvi
+import pynvi, pytest
 
 
 class TestVerifyIdentity:
     @classmethod
     def setup_class(cls):
-        cls.id_number = "11111111111"
+        cls.id_number = 11111111111
         cls.name = "John"
         cls.surname = "Doe"
-        cls.year_of_birth = "1994"
+        cls.year_of_birth = 1994
 
     def test_callable(self):
         assert callable(pynvi.verify_identity)
 
     def test_has_doc(self):
         assert pynvi.verify_identity.__doc__ is not None
+
+    def test_exists(self, mocker):
+        mocker.patch("pynvi.requests.post")
+
+        pynvi.requests.post.return_value.status_code = 200
+        with open("resources/verified_identity.xml", "r") as f:
+            pynvi.requests.post.return_value.text = f.read()
+
+        val = pynvi.verify_identity(
+            self.id_number, self.name, self.surname, self.year_of_birth
+        )
+
+        assert val
+
+    def test_not_exists(self, mocker):
+        mocker.patch("pynvi.requests.post")
+
+        pynvi.requests.post.return_value.status_code = 200
+        with open("resources/unverified_identity.xml", "r") as f:
+            pynvi.requests.post.return_value.text = f.read()
+
+        val = pynvi.verify_identity(
+            self.id_number, self.name, self.surname, self.year_of_birth
+        )
+
+        assert not val
+
+    def test_error(self, mocker):
+        mocker.patch("pynvi.requests.post")
+
+        pynvi.requests.post.return_value.status_code = 500
+        with open("resources/error_verify_identity.xml", "r") as f:
+            pynvi.requests.post.return_value.text = f.read()
+
+        with pytest.raises(Exception):
+            pynvi.verify_identity(self.id_number, self.name, self.surname, "0")
