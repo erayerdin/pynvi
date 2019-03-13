3af35790e013cf01381b208322ef8f6ce9b4b990
Eray Erdin <eraygezer.94@gmail.com>
2019-03-13 19:07:30 +0300

    wrapped some strings with textwrap.dedent

--- pynvi/__init__.py
+++ pynvi/__init__.py
@@ -4,7 +4,7 @@ Müdürlüğü SOAP servisi için oluşturulmuş köprü bir Python
 kütüphanesidir.
 """
 
-import requests
+import requests, textwrap
 
 __author__ = "Eray Erdin"
 __version__ = "0.1.0pre4"
@@ -20,16 +20,17 @@ class NVIException(Exception):
     def __init__(self, response: requests.Response):
         self.response = response
         self.message = """
-NVI server could not process the request properly.
+        NVI server could not process the request properly.
 
-Response Code: {code}
+        Response Code: {code}
 
-Response Content
-----------------
-{content}
+        Response Content
+        ----------------
+        {content}
         """.strip().format(
             code=response.status_code, content=response.text
         )
+        self.message = textwrap.dedent(self.message)
         super().__init__(self.message)
 
 
@@ -47,23 +48,24 @@ def verify_identity(
     year_of_birth = int(year_of_birth)
 
     request_body = """
-<?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-  <soap12:Body>
-    <TCKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
-      <TCKimlikNo>{identity_number}</TCKimlikNo>
-      <Ad>{name}</Ad>
-      <Soyad>{surname}</Soyad>
-      <DogumYili>{year_of_birth}</DogumYili>
-    </TCKimlikNoDogrula>
-  </soap12:Body>
-</soap12:Envelope>
+    <?xml version="1.0" encoding="utf-8"?>
+    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+      <soap12:Body>
+        <TCKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
+          <TCKimlikNo>{identity_number}</TCKimlikNo>
+          <Ad>{name}</Ad>
+          <Soyad>{surname}</Soyad>
+          <DogumYili>{year_of_birth}</DogumYili>
+        </TCKimlikNoDogrula>
+      </soap12:Body>
+    </soap12:Envelope>
     """.strip().format(
         identity_number=identity_number,
         name=name,
         surname=surname,
         year_of_birth=year_of_birth,
     )
+    request_body = textwrap.dedent(request_body)
     response = requests.post(
         URL,
         data=request_body.encode("utf-8"),
